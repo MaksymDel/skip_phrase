aed207626a0fb708a09a510c61ebb9148ba44037
Max Del <max.del.edu@gmail.com>
2018-02-11 18:14:04 +0200

    Add debug check

--- skip_phrase/models/skip_phrase.py
+++ skip_phrase/models/skip_phrase.py
@@ -109,7 +109,7 @@ class SkipPhrase(Model):
         print("loss neg ex iNF:", loss_negative_examples[numpy.isinf(loss_negative_examples.data).cuda()])
 
         # TODO: TO DELELTE - DEBUG
-        tmp = loss_negative_examples.squeeze().sigmoid()
+        tmp = loss_negative_examples.squeeze().sigmoid().log()
         print("TMP loss neg ex iNF SUM:", numpy.isinf(tmp.data).cuda().sum())
         print("TMP loss neg ex iNF:", tmp[numpy.isinf(tmp.data).cuda()])
 
